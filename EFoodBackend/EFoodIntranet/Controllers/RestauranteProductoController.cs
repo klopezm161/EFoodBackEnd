6f999c6cff6c692e63100fe0d1649b2831a8fb2d
Karla <klopezm161@ulacit.ed.cr>
2020-07-21 21:31:51 -0600

    actualización con ver producto para restaurante

---
+++ EFoodBackend/EFoodIntranet/Controllers/RestauranteProductoController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BLL;
+
+namespace EFoodIntranet.Controllers
+{
+    public class RestauranteProductoController : ApiController
+    {
+        // GET: api/RestauranteProducto
+        public string Get(string linea, string producto )
+        {
+            return new RestauranteProductoVer().carga_lista_Productos(linea, producto);
+
+        }
+
+        // GET: api/RestauranteProducto/5
+       
+    }
+}
