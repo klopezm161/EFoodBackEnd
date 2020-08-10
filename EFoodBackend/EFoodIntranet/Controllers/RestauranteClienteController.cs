982b55cdde80806bde7116647d5bb12af0726ffb
Karla <klopezm161@ulacit.ed.cr>
2020-08-09 18:55:13 -0600

    Parte cliente

---
+++ EFoodBackend/EFoodIntranet/Controllers/RestauranteClienteController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BLL;
+
+namespace EFoodIntranet.Controllers
+{
+    public class RestauranteClienteController : ApiController
+    {
+        // POST api/values      
+        public HttpResponseMessage Post([FromBody] RestauranteCliente cliente)
+        {
+            if (cliente.agregarCliente("Insertar"))
+            {
+                var message = string.Format("Se guardó con éxito el cliente");
+                return Request.CreateResponse(HttpStatusCode.OK, message);
+            }
+            else
+            {
+                var message = string.Format("No se guardó el cliente, verifique los datos.");
+                return Request.CreateResponse(HttpStatusCode.NotAcceptable, message);
+            }
+        }
+    }
+}
